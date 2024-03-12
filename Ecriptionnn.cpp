4938710df068dd9021ffb0ef6ec9fddf99abfb12
LAZZARETTO RUBEN <8293365@itisrossi.edu.it>
2024-03-12 09:00:09 +0100

    second

--- Ecriptionnn.cpp
+++ Ecriptionnn.cpp
@@ -1,3 +1,4 @@
+#define PATH "C:\\Users\\8293365\\tpsitLazzaretto\\ecription\\"
 #define BYTE 8
 #include <iostream>
 #include <thread>
@@ -11,10 +12,10 @@ struct word{
     char w[BYTE];};
 
     void WriteFile();
-    void ReadFile(std::string filename);//reads teh file 8 bytes at a time and loads the list1
+    int ReadFile(std::string filename);//reads teh file 8 bytes at a time and loads the list1
 
     //underground function
-    void ReadList(list<word> l);
+    void ReadList(std::list<word> l);
 
 
 
@@ -22,17 +23,46 @@ std::list<word> list1;
 std::list<word> list2;
 
 int main(){
+std::string nf;
+std::cout <<"\nenter file name\n";
+std::cin>>nf;
+ReadFile(nf);
+
+std::cout<<"\nNumber of writen words"<<list1.size()<<std::endl;
+
+}
 
 
+int ReadFile(std::string filename){//is called by the first thread
+    std::ifstream file;//let's see ho it needs to be made
+
+    word p1;//struct service
+    std::cout<<PATH+filename<<std::endl;
+    file.open(PATH+filename);
+
+
+    if(!file){
+    std::cout <<"\nProblem with file opening"<<std::endl;
+    return -1;//it is much better to be able to return a -1 to the main otherwise it would not be possible to check the state
+    }
+    char c;
+    int counter;
+    while(!file.eof()){
+
+        c = file.get();//get function reads along the file automatically
+        p1.w[counter] = c;
+        counter++;
+        if(counter == BYTE){//this condition statement controls whether or not we have reached the 8 byte limit alredy
+            counter = 0;
+            list1.push_back(p1);
+        }
+
+    }
 }
 
 
-void ReadFile(std::string filename){//is called by the first thread
-    ifstream file;//let's see ho it needs to be made
 
-    word p1;//struct service 
 
-}
 
 
 
